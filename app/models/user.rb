c046ee588e0f364e4f0ca7a7cc7c408b85d006e9
KevinSia <kevinsiakeemun@gmail.com>
2016-02-22 18:19:33 +0800

    done exercise 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,8 +30,7 @@ class User < ActiveRecord::Base
 
 
 	def activate
-		update_attribute(:activated, true)
-		update_attribute(:activated_at, Time.zone.now)
+		update_columns(activated: true, activated_at: Time.zone.now)
 	end
 
 	# matches with the incoming cookie's remember_token
@@ -44,8 +43,7 @@ class User < ActiveRecord::Base
 
 	def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
 	# removes token associated to user
--- test/integration/password_reset_test.rb
+++ test/integration/password_reset_test.rb
@@ -72,6 +72,12 @@ class PasswordResetTest < ActionDispatch::IntegrationTest
       post password_resets_path, password_reset: { email: @user.email }
       @user = assigns(:user) # get model object
       @user.update_attribute(:reset_sent_at, 3.hours.ago)
+      patch password_reset_path(@user.reset_token), email: @user.email,
+                                                    user: { password: "foobaz",
+                                               password_confirmation: "foobaz" }
+      assert_response :redirect
+      follow_redirect!
+      assert_match /expired/i, response.body
     end
 
 end
